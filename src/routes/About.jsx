469a726a55c3582021a0ddd373cf5030411d570b
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-08 12:50:57 +0530

    About route complete

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,10 +1,24 @@
+import "./About.css";
+
 export default function About() {
     return (
-        <>
-            <h1>This is About Page</h1>
+        <div className="about-page">
+            <h2>About Us</h2>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                At Cart-UP! Store, we are passionate about bringing you the best
+                shopping experience. Our mission is to provide a wide range of
+                high-quality products, exceptional customer service, and
+                affordable prices. With a dedicated team of experts, we
+                carefully curate our collection to offer you the latest trends
+                and must-have items.
             </p>
-        </>
-    )
+            <p>
+                We believe that shopping should be enjoyable and convenient.
+                Whether you are looking for fashion, home decor, electronics, or
+                more, we have got you covered. Our user-friendly website and
+                secure checkout process ensure a seamless shopping journey.
+            </p>
+            <p>Thank you for choosing Cart-UP! Store. Happy shopping!</p>
+        </div>
+    );
 }
