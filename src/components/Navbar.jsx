29f497742342f3144023016934807e4f2f24c1da
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-08 12:17:40 +0530

    Added Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
+    const location = useLocation();
+    const [activeLink, setActiveLink] = useState("");
+
+    useEffect(() => {
+        // Get the current path from the location object
+        const currentPath = location.pathname;
+
+        // Update the activeLink state based on the current path
+        setActiveLink(currentPath);
+    }, [location]);
+
     return (
         <nav>
             <ul>
-                <li>
+                <li className={activeLink === "/" ? "active" : ""}>
                     <Link to="/">Home</Link>
                 </li>
-                <li>
+                <li className={activeLink === "/shop" ? "active" : ""}>
                     <Link to="/shop">Shop</Link>
                 </li>
-                <li>
+                <li className={activeLink === "/about" ? "active" : ""}>
                     <Link to="/about">About Us</Link>
+                </li>
+                <li className={activeLink === "/cart" ? "active" : ""} id="cart">
+                    <Link to="/cart">Your Cart</Link>
                 </li>
             </ul>
         </nav>
