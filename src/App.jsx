4c9c4c3094b14e306156ac0fb32c973db4510872
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-08 23:15:29 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 import "./App.css";
 import { Outlet } from "react-router-dom";
@@ -13,6 +14,7 @@ function App() {
             <h1 className="title">Cart-UP!</h1>
             <Navbar />
             <Outlet context={[userCart, setUserCart]} />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer>
+            <span>
+                Built by <a href="https://github.com/r0hitm">Rohit Metha</a>
+            </span>
+            <span>
+                View on
+                <a href="https://github.com/r0hitm/shopping-kart">GitHub</a>
+            </span>
+        </footer>
+    );
+}
