b302fead40f9beadfb58a1a7b042f312bf4662f6
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-08 12:46:34 +0530

    HomePage complete

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,18 +2,13 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 
 export default function Home() {
-    return <h2>Home</h2>;
+    return (
+        <div className="homepage">
+            <h2>Welcome to Cart-UP! Store</h2>
+            <p>Discover a world of amazing products at unbeatable prices.</p>
+            <Link to="/shop" className="shop-button">
+                Start Shopping
+            </Link>
+        </div>
+    );
 }
-
-// import { Link } from "react-router-dom";
-// import "./Homepage.css";
-
-// export default function Homepage() {
-//     return (
-//         <div className="homepage">
-//             <h1>Welcome to Cart-UP! Store</h1>
-//             <p>Discover a world of amazing products at unbeatable prices.</p>
-//             <Link to="/shop" className="shop-button">Start Shopping</Link>
-//         </div>
-//     );
-// }
