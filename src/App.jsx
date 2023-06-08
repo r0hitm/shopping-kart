d76333213f86fb5a84047001fe7e4dab5862f266
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-06-08 12:18:45 +0530

    New Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 // import { useState } from 'react'
 // import cartLogo from '/cart.svg'
 
-import Navbar from './components/Navbar.jsx'
+import Navbar from "./components/Navbar.jsx";
 
-// import './App.css'
-import { Outlet } from 'react-router-dom'
+import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <h1>This is Main page with children routes</h1>
-      <Navbar />
-      <Outlet />
-    </>
-  )
+    return (
+        <>
+            <h1 className="title">Cart-UP!</h1>
+            <Navbar />
+            <Outlet />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,10 +1,3 @@
 export default function Home() {
-    return (
-        <>
-            <h1>This is Home Page</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            </p>
-        </>
-    )
+    return <h2>Home</h2>;
 }
